681170b49a5e2e469b7e6102ea5c97b576e44b53
reinaldoper <reinaldoper83@gmail.com>
2025-01-30 11:49:05 -0400

    feat: Ajustando o readme

--- datas/tecnologias.js
+++ datas/tecnologias.js
@@ -47,6 +47,18 @@ const tecnologias = [
     descricao: 'Java with Spring Boot framework',
     src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg',
   },
+  {
+    id: 9,
+    titulo: 'Spring Boot',
+    descricao: 'Spring Boot is a powerful framework for building Java applications.',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg',
+  },
+  {
+    id: 10,
+    titulo: 'Python',
+    descricao: 'Python with Django framework',
+    src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
+  }
 ];
 
 export default tecnologias;
